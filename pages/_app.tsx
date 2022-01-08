7ed59daedd1cf3ddbe8d133e9b206053477a8bb5
Jin Haneul <knsan189@naver.com>
2022-01-08 19:41:03 +0900

    cherry pick deveop

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,36 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { NextPage } from "next";
-import { ThemeProvider } from "@emotion/react";
-import useCustomTheme from "../src/hooks/useCustomTheme";
+import * as React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppProps } from "next/app";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import createEmotionCache from "../src/createEmotionCache";
 import wrapper from "../src/redux/store/wrapper";
+import useCustomTheme from "../src/hooks/useCustomTheme";
 
-const MyApp: NextPage<AppProps> = ({ Component, pageProps }: AppProps) => {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+const MyApp = (props: MyAppProps) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   const { theme } = useCustomTheme();
 
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
+};
+
+MyApp.defaultProps = {
+  emotionCache: undefined,
 };
 
 export default wrapper.withRedux(MyApp);

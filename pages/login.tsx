b446fba419bc37dadec5595f62d9190cf13b425b
Jin Haneul <knsan189@naver.com>
2022-01-08 19:41:03 +0900

    remove custom components, add mui/material

---
+++ pages/login.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const login: NextPage = () => {
+  return <div>a</div>;
+};
+
+export default login;
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,64 +1,62 @@
-import { CSSObject, useTheme } from "@emotion/react";
-import styled from "@emotion/styled";
-import { ArrowBack } from "@mui/icons-material";
+import { ArrowBack, Home, Search } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useRouter } from "next/router";
 import React, { useRef } from "react";
-import Box from "./Box";
-import Button from "./Button";
-import Icon from "./Icon";
-import IconButton from "./IconButton";
-import Typo from "./Typo";
-
-const getBoxStyle = (): CSSObject => ({
-  marginBottom: 16,
-  "&>*": {
-    marginRight: 8,
-  },
-});
 
 const Hello = () => {
-  const theme = useTheme();
-  const testRef = useRef<HTMLButtonElement>(null);
-  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // eslint-disable-next-line no-console
-    console.log(e);
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/login");
   };
-
   return (
     <Box display="flex" justifyContent="center" alignItems="center" sx={{ height: "100vh" }}>
-      <Box>
-        <Box
-          sx={{
-            boxShadow: theme.shadows[2],
-            borderRadius: theme.shape.borderRadius,
-            padding: theme.spacing(2),
-            marginBottom: theme.spacing(2),
-          }}
-        >
-          <Typo variant="subtitle">subtitle : Hello it's String Cat!</Typo>
-          <Typo variant="body">body : Hello it's String Cat!</Typo>
-          <Typo variant="body2">body2 : Hello it's String Cat!</Typo>
-          <Typo variant="caption">caption : Hello it's String Cat!</Typo>
-        </Box>
-        <Box sx={getBoxStyle()}>
-          <Button ref={testRef} onClick={onClick} variant="disabled">
+      <AppBar>
+        <Toolbar>
+          <Container>
+            <Button onClick={onClickButton} color="secondary">
+              로그인 하러가기
+            </Button>
+          </Container>
+        </Toolbar>
+      </AppBar>
+      <Card>
+        <CardContent>
+          <Typography variant="subtitle1">subtitle : Hello it's String Cat!</Typography>
+          <Typography variant="body1">body : Hello it's String Cat!</Typography>
+          <Typography variant="body2">body2 : Hello it's String Cat!</Typography>
+          <Typography variant="caption">caption : Hello it's String Cat!</Typography>
+        </CardContent>
+
+        <CardContent>
+          <Button variant="contained">테스트</Button>
+          <Button variant="contained" color="secondary">
             테스트
           </Button>
-          <Button ref={testRef} onClick={onClick}>
-            테스트
-          </Button>
-        </Box>
-        <Box display="flex" sx={getBoxStyle()}>
+        </CardContent>
+
+        <CardContent>
           <IconButton>
-            <Icon type="search" />
+            <ArrowBack />
           </IconButton>
-          <IconButton variant="filled">
-            <Icon type="search" />
+          <IconButton>
+            <Search />
           </IconButton>
-          <IconButton variant="disabled">
-            <Icon type="search" />
+          <IconButton>
+            <Home />
           </IconButton>
-        </Box>
-      </Box>
+        </CardContent>
+      </Card>
     </Box>
   );
 };

6866a4bea6f8bce5f7a94a5cdc3e3c794464b85c
Jin Haneul <knsan189@naver.com>
2022-02-06 21:45:15 +0900

    add SearchBar component

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,14 +1,58 @@
-import { AppBar, Button, Container, Toolbar } from "@mui/material";
+import { ArrowBack, Home, NotificationsActive, SupervisedUserCircle } from "@mui/icons-material";
+import { AppBar, Box, Button, styled, TextField, Toolbar } from "@mui/material";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { MouseEventHandler } from "react";
+import SearchBar from "../questions/SearchBar";
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  fontSize: "16px",
+}));
+
+const Container = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  paddingLeft: "100px",
+  paddingRight: "52px",
+  margin: "0 auto",
+  maxWidth: 1200,
+  width: "100%",
+}));
 
 const Header = () => {
+  const router = useRouter();
+  const onClickBack: MouseEventHandler = () => {
+    router.back();
+  };
+
   return (
     <AppBar elevation={0} sx={{ background: (theme) => theme.palette.background.default }}>
-      <Toolbar>
+      <Toolbar sx={{ minHeight: 50 }} disableGutters variant="dense">
         <Container>
+          <StyledButton onClick={onClickBack}>
+            <ArrowBack />
+          </StyledButton>
+          <Link passHref href="/">
+            <StyledButton>
+              <Home />
+            </StyledButton>
+          </Link>
+          <Link passHref href="/users">
+            <StyledButton>
+              <SupervisedUserCircle />
+            </StyledButton>
+          </Link>
+          <SearchBar />
+          <StyledButton>
+            <NotificationsActive />
+          </StyledButton>
+          <Link passHref href="/mypage">
+            <StyledButton>마이페이지</StyledButton>
+          </Link>
           <Link passHref href="/auth/login">
-            <Button color="secondary">로그인 하러가기</Button>
+            <StyledButton>로그인</StyledButton>
           </Link>
         </Container>
       </Toolbar>
